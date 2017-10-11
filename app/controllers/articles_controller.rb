0a339528dfb15f54e26092f8b11a72736181993a
wangfeifei <>
2017-10-11 22:35:27 +0800

    create2

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,9 +3,6 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(params[:article])
-
-    @article.save
-    redirect_to @article
+    @article = Article.new(params.require(:article).permit(:title, :text))
   end
 end
